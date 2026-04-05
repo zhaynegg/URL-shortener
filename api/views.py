63d6c5397f7eb917668ae1e4675e46e4907ce941
Zhassyn Yegnay <zhasynea@gmail.com>
2026-04-05 22:08:23 +0500

    created template:

--- api/views.py
+++ api/views.py
@@ -1,13 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404, HttpResponse
 from .models import urls
 
 def index(request):
-    return HttpResponse("Hello. This is the main page where you are going to shorten URLs")
+    return render(request, 'api/index.html')
 
 def analytics(request, url_id):
-    try:
-        info = urls.objects.get(id=url_id)
-        return HttpResponse(f"This is the analytics page for URL used count: {info.used_count} where you can see the statistics of your shortened URLs")  
-    except urls.DoesNotExist:
-        raise Http404("URL not found")
+    info = get_object_or_404(urls, id=url_id)
+    return HttpResponse(f"This is the analytics page for URL used count: {info.used_count} where you can see the statistics of your shortened URLs")

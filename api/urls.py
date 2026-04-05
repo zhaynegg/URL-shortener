f97f568e53477d45c8b9e97b32e43592eae50e98
Zhassyn Yegnay <zhasynea@gmail.com>
2026-04-05 23:22:10 +0500

    implemented redirect

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'api'
 urlpatterns = [
     path('', views.index, name='index'),
     path('analytics/<int:url_id>/', views.analytics, name='analytics'),
+    path('redirect/<str:short_url>/', views.redirect, name='redirect'),
+    path('create/', views.create_short_url, name='create_short_url'),  
 ]
--- api/views.py
+++ api/views.py
@@ -8,3 +8,20 @@ def index(request):
 def analytics(request, url_id):
     info = get_object_or_404(urls, id=url_id)
     return HttpResponse(f"This is the analytics page for URL used count: {info.used_count} where you can see the statistics of your shortened URLs")
+
+def redirect(request, short_url):
+    try:
+        url_info = urls.objects.get(short_url=short_url)
+        url_info.used_count += 1
+        url_info.save()
+        return render(request, 'api/redirect.html', {'original_url': url_info.original_url})
+    except urls.DoesNotExist:
+        raise Http404("Short URL does not exist") 
+    
+def create_short_url(request):
+    if request.method == 'POST':
+        original_url = request.POST.get('longurl')
+        if original_url:
+            url_info = urls.objects.create(original_url=original_url)
+            return HttpResponse(f"Short URL created: {request.build_absolute_uri('/')}{url_info.short_url}")
+    return render(request, 'api/index.html')

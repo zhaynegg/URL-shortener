ebcbeb95f4efb1e98e7f51ea1adfbab49a80f685
Zhassyn Yegnay <zhasynea@gmail.com>
2026-04-06 13:25:38 +0500

    implemented simple short url generator

--- api/urls.py
+++ api/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 app_name = 'api'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('analytics/<int:url_id>/', views.analytics, name='analytics'),
-    path('redirect/<str:short_url>/', views.redirect, name='redirect'),
-    path('create/', views.create_short_url, name='create_short_url'),  
+    path('', views.IndexView.as_view(), name='index'),
+    path('analytics/<int:pk>/', views.AnalyticsView.as_view(), name='analytics'),
+    path('redirect/<str:short_url>/', views.RedirectView.as_view(), name='redirect'),
+    path('create/', views.create_short_url, name='create_short_url'), 
+    path('shortened/<str:short_url>/', views.ShortenedURLView.as_view(), name='shortened_url'),
 ]
--- api/views.py
+++ api/views.py
@@ -1,27 +1,48 @@
+import random
+import string
+from urllib import request
+
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404, HttpResponse
+from django.urls import reverse
 from .models import urls
+from django.views import generic
 
-def index(request):
-    return render(request, 'api/index.html')
+class IndexView(generic.TemplateView):
+    template_name = 'api/index.html'
 
-def analytics(request, url_id):
-    info = get_object_or_404(urls, id=url_id)
-    return HttpResponse(f"This is the analytics page for URL used count: {info.used_count} where you can see the statistics of your shortened URLs")
+class AnalyticsView(generic.DetailView):
+    model = urls
+    template_name = 'api/analytics.html'
 
-def redirect(request, short_url):
-    try:
-        url_info = urls.objects.get(short_url=short_url)
-        url_info.used_count += 1
-        url_info.save()
-        return render(request, 'api/redirect.html', {'original_url': url_info.original_url})
-    except urls.DoesNotExist:
-        raise Http404("Short URL does not exist") 
-    
+class RedirectView(generic.View):
+    def get(self, request, short_url):
+        try:
+            url_info = urls.objects.get(short_url=short_url)
+            url_info.used_count += 1
+            url_info.save()
+            return render(request, 'api/redirect.html', {'original_url': url_info.original_url})
+        except urls.DoesNotExist:
+            raise Http404("Short URL does not exist")
+        
+
+def generate_short_url():
+    short_url = ""
+    for x in range(6):
+        short_url += random.choice(string.ascii_letters + string.digits)
+    return short_url
+
 def create_short_url(request):
     if request.method == 'POST':
-        original_url = request.POST.get('longurl')
+        original_url = request.POST.get('original_url')
         if original_url:
-            url_info = urls.objects.create(original_url=original_url)
-            return HttpResponse(f"Short URL created: {request.build_absolute_uri('/')}{url_info.short_url}")
-    return render(request, 'api/index.html')
+            short_url = generate_short_url()
+            url_info = urls.objects.create(original_url=original_url, short_url=short_url)
+            return HttpResponse(reverse('api:shortened_url', args=[url_info.short_url]))
+        return render(request, 'api/index.html')
+    
+class ShortenedURLView(generic.DetailView):
+    model = urls
+    template_name = 'api/shortened_url.html'
+    slug_field = 'short_url'
+    slug_url_kwarg = 'short_url'

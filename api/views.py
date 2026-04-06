5a457c84e9940bcfc81fd3e2fdb76f341194db86
Zhassyn Yegnay <zhasynea@gmail.com>
2026-04-06 19:06:48 +0500

    created short url generation

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
 import random
 import string
+import xxhash
 from urllib import request
 
 from django.shortcuts import redirect, render, get_object_or_404
@@ -26,19 +27,24 @@ class RedirectView(generic.View):
         except urls.DoesNotExist:
             raise Http404("Short URL does not exist")
 
-def generate_short_url():
-    short_url = ""
-    for x in range(6):
-        short_url += random.choice(string.ascii_letters + string.digits)
+def generate_short_url(original_url):
+    short_url = xxhash.xxh3_128_hexdigest(original_url)[:8]
+    while urls.objects.filter(short_url = short_url).exists():
+        original_url += random.choice(string.ascii_letters + string.digits)
+        short_url = xxhash.xxh3_128_hexdigest(original_url)[:8]
     return short_url
 
 def create_short_url(request):
     if request.method == 'POST':
         original_url = request.POST.get('original_url')
         if original_url:
-            short_url = generate_short_url()
-            url_info = urls.objects.create(original_url=original_url, short_url=short_url)
+            if urls.objects.filter(original_url = original_url).exists():
+                url_info = urls.objects.get(original_url = original_url)
+            else:
+                short_url = generate_short_url(original_url)
+                url_info = urls.objects.create(original_url=original_url, short_url=short_url)
             return render(request, 'api/shortened_url.html', {'url': url_info})
+        
         return render(request, 'api/index.html')
     
 class ShortenedURLView(generic.View):

affc402d139c56a293396b36d863c747d0df59bc
Zhassyn Yegnay <zhasynea@gmail.com>
2026-04-06 13:49:38 +0500

    redirect works

--- api/views.py
+++ api/views.py
@@ -2,7 +2,7 @@ import random
 import string
 from urllib import request
 
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404
 from django.http import Http404, HttpResponse
 from django.urls import reverse
 from .models import urls
@@ -21,7 +21,7 @@ class RedirectView(generic.View):
             url_info = urls.objects.get(short_url=short_url)
             url_info.used_count += 1
             url_info.save()
-            return render(request, 'api/redirect.html', {'original_url': url_info.original_url})
+            return redirect(url_info.original_url)
         except urls.DoesNotExist:
             raise Http404("Short URL does not exist")
         
@@ -38,7 +38,7 @@ def create_short_url(request):
         if original_url:
             short_url = generate_short_url()
             url_info = urls.objects.create(original_url=original_url, short_url=short_url)
-            return HttpResponse(reverse('api:shortened_url', args=[url_info.short_url]))
+            return render(request, 'api/shortened_url.html', {'url': url_info})
         return render(request, 'api/index.html')
     
 class ShortenedURLView(generic.View):

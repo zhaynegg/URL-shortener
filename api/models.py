fdbaed2f108a390aba8aaa5bf307deb311ba255d
Zhassyn Yegnay <zhasynea@gmail.com>
2026-04-07 17:21:45 +0500

    do not allow null values in user_username

--- api/models.py
+++ api/models.py
@@ -5,7 +5,7 @@ class urls(models.Model):
     original_url = models.URLField()
     short_url = models.CharField(max_length=10, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    user_username = models.CharField(max_length=150, null=True)
+    user_username = models.CharField(max_length=150, null=False)
     used_count = models.IntegerField(default=0)
 
     def __str__(self):
